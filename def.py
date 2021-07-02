b9b445a414b53961665531b6215fbfb2c49bfa38
Jacob <jelondon12@gmail.com>
2021-07-02 15:10:36 -0700

    Improve readability

--- def.py
+++ def.py
@@ -59,6 +59,41 @@ def arg_get(argv, da, ddarg):
         return argv[ndx + 1]
     return None
 
+def enforce(item, istype):
+    if type(item) is not istype:
+        print("%s is not a %s" % (item, str(istype)), file=sys.stderr)
+        exit(1)
+
+def read_json(filename):
+    try:
+        with open(filename, "r") as fp:
+            return json.load(fp)
+    except Exception as e:
+        print("Could not read JSON %s: %s" % (filename, e), file=sys.stderr)
+        exit(1)
+
+def read_file(filename):
+    try:
+        with open(filename, "r") as fp:
+            return fp.read()
+    except:
+        print("Could not read file %s" % (filename,), file=sys.stderr)
+        exit(1)
+
+def write_file(filename, stuff):
+    try:
+        with open(filename, "w") as fp:
+            if type(stuff) is list:
+                for text in stuff:
+                    fp.write(text)
+            elif type(stuff) is str:
+                fp.write(stuff)
+            else:
+                raise NotImplementedError
+    except:
+        print("Could not write file %s" % (filename,), file=sys.stderr)
+        exit(1)
+
 def main():
     file = None
     out = None
@@ -67,75 +102,43 @@ def main():
         print("-- no options specified", file=sys.stderr)
         usage()
         return 1
-
     if arg_check(sys.argv, '-h', '--help'):
         usage()
         return 0
-
     tmp = arg_get(sys.argv, '-f', '--file')
     if tmp is not None:
         file = tmp
     else:
         print("Missing --file", file=sys.stderr)
         return 1
-
     tmp = arg_get(sys.argv, '-o', "--output")
     if tmp is not None:
         out = tmp
     else:
-        print("Missing --out", file=sys.stderr)
+        print("Missing --output", file=sys.stderr)
         return 1
 
-    try:
-        with open(file, "r") as fp:
-            obj = json.load(fp)
-    except Exception as e:
-        print(f"Could not read JSON {file}: {e}", file=sys.stderr)
-        return 1
-
-    if type(obj) is not dict:
-        print("Invalid JSON: File is not an object", file=sys.stderr)
-        return 1
+    obj = read_json(file)
+    enforce(obj, dict)
 
     converted_texts = []
     for filename, conversion_list in obj.items():
-        if type(filename) is not str:
-            print(f"{filename} is not a string", file=sys.stderr)
-            return 1
-        elif type(conversion_list) is not list:
-            print(f"{conversion_list} is not a list", file=sys.stderr)
+        enforce(filename, str)
+        enforce(conversion_list, list)
 
-        try:
-            with open(filename, "r") as fp:
-                text = fp.read()
-        except:
-            print(f"Could not open input file {filename}", file=sys.stderr)
-            return 1
-
+        text = read_file(filename)
         for conversion in conversion_list:
-            if type(conversion) is not dict:
-                print(f"Conversion {conversion} is not an object", file=sys.stderr)
-                return 1
+            enforce(conversion, dict)
 
             ptext = text
             for unconv, conv in conversion.items():
-                if type(unconv) is not str:
-                    print(f"Unconverted item {unconv} is not a string", file=sys.stderr)
-                    return 1
-                elif type(conv) is not str:
-                    print(f"Converted item {conv} is not a string", file=sys.stderr)
-                    return 1
+                enforce(unconv, str)
+                enforce(conv, str)
 
                 ptext = ptext.replace(unconv, conv)
             converted_texts.append(ptext)
 
-    try:
-        with open(out, "w") as fp:
-            for text in converted_texts:
-                fp.write(text)
-    except:
-        print(f"Could not open output file {filename}", file=sys.stderr)
-        return 1
+    write_file(out, converted_texts)
     return 0
 
 if __name__ == '__main__':
